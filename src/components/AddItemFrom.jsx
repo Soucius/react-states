efdbd88b9458d7f52764f4b1ff9d253278d9b4a5
Soucius <soucius.ggwp@gmail.com>
2025-05-14 13:52:17 +0200

    form data

--- src/components/AddItemFrom.jsx
+++ src/components/AddItemFrom.jsx
@@ -1,12 +1,35 @@
+import { useState } from "react";
+
 export default function AddItemForm() {
+  const [name, setName] = useState("");
+
+  function handleFormSubmit(e) {
+    e.preventDefault();
+
+    const item = {
+      id: Date.now(),
+      name,
+      completed: false,
+    };
+
+    console.log(item.name);
+
+    setName("");
+  }
+
   return (
-    <form className="shopping-form border rounded p-3 mb-3">
+    <form
+      className="shopping-form border rounded p-3 mb-3"
+      onSubmit={handleFormSubmit}
+    >
       <div className="input-group">
         <input
           type="text"
           id="item_name"
           className="form-control"
           placeholder="Add Item"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
 
         <button type="submit" className="btn btn-primary">
